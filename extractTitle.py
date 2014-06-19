9f93b09441f8e04e44bfac0c5f6cc7fb0a18bcd5
devchenpy <chenwennothing@126.com>
2014-06-19 16:33:57 +0800

    抽取网页标题
    用lxml取出的时unicode，写入文件时，要转为utf-8，否则会报错

---
+++ extractTitle.py
@@ -0,0 +1,34 @@
+#coding:utf-8
+import urllib2
+import lxml
+import csv
+from lxml import *
+import lxml.html
+from urllib2 import HTTPError
+import codecs
+
+
+def main():
+	csvRead = file('tieba1.csv','rb')
+	reader = csv.reader(csvRead)
+
+	csvWrite = open('result.csv',"wb")
+	csvWrite.write(codecs.BOM_UTF8)
+	writer = csv.writer(csvWrite,quoting=csv.QUOTE_ALL)
+
+	for line in reader:
+		try:
+			html = urllib2.urlopen(line[1]).read()
+			doc = lxml.html.document_fromstring(html)
+			title = doc.xpath("/html/head/title/text()")
+			print "url = "+line[1]+"'s title is "+ title[0]
+			l = [line[0],line[1],title[0].encode('utf-8')]
+			writer.writerow(l)
+		except Exception, e:
+			print e
+	csvRead.close()
+	csvWrite.close()
+
+if __name__ == '__main__':
+	main()
+
